723876ae40507309709a21b5af044ad830b7bee0
Salumao Barobsa da Costa <salubcosta@gmail.com>
2020-10-23 00:14:14 -0300

    Firsts commands in the  php languagem

---
+++ 01-saida-exemplo.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * echo - instrução de saída.
+ * echo aceita parâmetros
+ */
+echo 'Hello World';
+
+echo "Hello", " World";
+
+/**
+ * print - também é uma instrução de saída.
+ * Não aceita parâmetros
+ */
+
+ print 'Hello World';
