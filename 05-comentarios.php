286062f14c0827793804f304b63285acf6f6c60d
Salumao Barobsa da Costa <salubcosta@gmail.com>
2020-10-23 17:47:28 -0300

    file created to work with comments

---
+++ 05-comentarios.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Esta é uma forma de comentar mais uma linha.
+ * 
+ * Os comentários servem para você explicar determinado procedimento no software.
+ * 
+ * Os comentários não são interpretados.
+ */
+
+ /*
+    Funciona exatamente como o comentário anterior
+ */
+
+ // Comentário de uma linha
+
+ # Comentário de uma linha.
+
