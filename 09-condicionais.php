41e886923ae8235b6f75f4dc0a40ed5fde4ff130
Salumao Barobsa da Costa <salubcosta@gmail.com>
2020-10-23 23:56:48 -0300

    Was included more implementation

--- 09-condicionais.php
+++ 09-condicionais.php
@@ -2,8 +2,46 @@
 
 $itsRains = false;
 
-if($itRains){
+if($itsRains){
     echo 'Stay home!';
 } else {
     echo 'Go to pool';
+}
+
+
+# Comparações
+
+$numberOne = 10;
+$numberTwo = 25;
+
+# Igualdade
+if($numberOne == $numberTwo){
+    echo "{$numberOne} == {$numberTwo}";
+}
+
+# Diferença
+if($numberOne != $numberTwo){
+    echo "{$numberOne} != {$numberTwo}";
+}
+
+# Maior que. Também pode ser usado assim: >= (maior ou igual)
+if($numberOne > $numberTwo){
+    echo "{$numberOne} > {$numberTwo}";
+}
+
+# Menor que. Também pode ser usado assim: <= (menor ou igual)
+if($numberOne < $numberTwo){
+    echo "{$numberOne} < {$numberTwo}";
+}
+
+/**
+ * O if pode ser "aninhado". Segue um exemplo:
+ */
+
+if($numberOne > $numberTwo){
+    echo "The value {$numberOne} is tall than {$numberTwo}";
+} elseif($numberOne < $numberTwo){
+    echo "The value {$numberOne} is less than {$numberTwo}";
+} else {
+    echo "The values are equals!";
 }
